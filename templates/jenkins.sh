27e8f8bcee7a199802aa67a786aeeb67ccaab36b
Gabor Csardi <csardi.gabor@gmail.com>
2016-02-09 17:07:32 +0000

    Jenkins script tweaks, need an env file for Docker

--- templates/jenkins.sh
+++ templates/jenkins.sh
@@ -2,6 +2,7 @@
 shfile=$(tempfile -s .sh)
 
 echo > "${shfile}" '
+cd ~
 export PATH=$(ls /opt/R-* -d)/bin:$PATH
 export R_LIBS=~/R
 mkdir -p ~/R
@@ -10,5 +11,11 @@ curl -o "$package" "$url"
 R CMD check "$package"
 '
 
-docker run -i --user docker rhub/debian-gcc-release \
+env=$(tempfile)
+echo url=$url >> $env
+echo package=$package >> $env
+
+docker run -i --user docker --env-file $env rhub/debian-gcc-release \
   /bin/bash -c 'cat > /tmp/build.sh; chmod +x /tmp/build.sh; /tmp/build.sh' < "${shfile}"
+
+rm -f $env
