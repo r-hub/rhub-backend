2f09c83631a8d01dc02c9a94aa52a4ed053b88e7
Gabor Csardi <csardi.gabor@gmail.com>
2016-02-09 16:48:09 +0000

    Run R CMD check on the submission

--- templates/jenkins.sh
+++ templates/jenkins.sh
@@ -6,7 +6,8 @@ export PATH=$(ls /opt/R-* -d)/bin:$PATH
 export R_LIBS=~/R
 mkdir -p ~/R
 echo "options(repos = c(CRAN = \"https://cran.rstudio.com/\"))" >> ~/.Rprofile
-R -e "install.packages(\"parsedate\")"
+wget -O "$package" "$url"
+R CMD check "$package"
 '
 
 docker run -i --user docker rhub/debian-gcc-release \
